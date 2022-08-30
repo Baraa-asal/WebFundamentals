24c65bb857999032ed00e2e9f2a3a68659e9af40
baraa-asal <b.aboasal@gmail.com>
2022-08-30 13:06:45 +0300

    Button Clicker

---
+++ JavaScript/Video Preview/video-preview/script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+
+function over(element){
+element.play();
+}
+function out(element){
+element.pause();
+}
+
+// document.getElementById("myVid").addEventListener("mouseleave", function() {
+// 	this.pause();
+// });
