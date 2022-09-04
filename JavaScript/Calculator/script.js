77e2e922ea99d278c676ee9d49fcbb54e59db240
baraa-asal <b.aboasal@gmail.com>
2022-09-04 16:59:54 +0300

    about python

--- JavaScript/Calculator/script.js
+++ JavaScript/Calculator/script.js
@@ -1,10 +1,54 @@
-var firstOperand = 0 , factor = 1;
-function press(key){
-    if(key == "." && factor == 1) factor = 0.1;
-    console.log(factor)
-    firstOperand = firstOperand*10 + key*factor;
-    console.log(firstOperand)
-
-    factor*=factor; 
-
+var firstOperand = '', secondOperand = '', operator = '' , dotFlag = false;
+var output = 0;
+function press(key) {
+    if (operator == '') {
+        firstOperand = setOperand(key, firstOperand);
+        putOnDisplay(firstOperand)
+    } else {
+       secondOperand = setOperand(key, secondOperand); 
+       putOnDisplay(secondOperand)
+    }
 }
+function setOP(op) {
+    operator = op;
+    dotFlag = false;
+    putOnDisplay(0)
+}
+function setOperand(key, value) {
+    if (key === '.' && dotFlag) return;
+    if (key === '.') dotFlag = true;
+    value = '' + value + key;
+    return value;
+}
+function reset() {
+    firstOperand =  '';
+    secondOperand = '';
+    operator = '';
+    dotFlag = '';
+    output = 0;
+}
+function clr() {
+    reset();
+    putOnDisplay(output);
+}
+function calculate() {
+    var firstOperandVal = parseFloat(firstOperand), secondOperandVal = parseFloat(secondOperand);
+    if (isNaN(firstOperandVal) || isNaN(secondOperandVal)) return;
+    if (output != 0) {
+        output = 0;
+    } else {
+        if (operator == '+') output = firstOperandVal + secondOperandVal;
+        if (operator == '-') output = firstOperandVal - secondOperandVal;
+        if (operator == '*') output = firstOperandVal * secondOperandVal;
+        if (operator == '/') output = firstOperandVal / secondOperandVal;
+    }
+    
+    putOnDisplay(output);
+    reset();
+}
+function putOnDisplay(value) {
+    document.querySelector("#display").innerHTML = value;
+}
+firstOperand (operator) secondOperand = 
+
+
